a85219771fb5bc3f499654caecd0939a3adaf2ca
Daiane Bushey <daianebushey@gmail.com>
2018-01-12 19:12:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,19 @@
 class School
-  #attr_accessor
   attr_reader :name, :roster
 
   def initialize(name)
     @name = name
-  end
-
-  def roster
     @roster = {}
   end
 
   def add_student(student_name, grade)
-    @roster[grade] = []
-    @roster[grade] << student_name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
 
-  def grade(grade)
+  def grade(student_grade)
     #returns an array with all students with that particular grade
+    roster[student_grade]
   end
 
 end
